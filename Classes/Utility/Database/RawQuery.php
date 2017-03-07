268208374908ed8a900f47452c78258ef1f56ca9
Ezra Ter Linden <terlinden@itplusx.de>
2017-03-07 16:19:54 +0100

    [TASK] Basic project structure

---
+++ Classes/Utility/Database/RawQuery.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types = 1);
+namespace Ooonoid\TYPO3\Utility\Database;
+
+/**
+ * Class RawQuery
+ *
+ *
+ *
+ * @package Ooonoid\TYPO3\Utility\Database
+ */
+class RawQuery
+{
+  //TODO: create nice wrappers for http://doc-typo3.ameos.com/4.1.0/classt3lib__DB.html
+}
